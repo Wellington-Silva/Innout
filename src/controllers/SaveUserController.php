cd0936277eec5bba77faae55674bc573257d9edf
Wellington-Silva <wellingtonsilva112000@gmail.com>
2025-10-15 15:18:59 -0300

    Create user

--- src/controllers/SaveUserController.php
+++ src/controllers/SaveUserController.php
@@ -3,18 +3,26 @@ session_start();
 requireValidSession();
 
 $exception = null;
+$userData = []; 
+$errors = [];
 
 if (count($_POST) > 0) {
+    $userData = $_POST; 
+    
     try {
        $newUser = new User($_POST);
        $newUser->insert();
        addSuccessMsg("Usuário cadastrado com sucesso");
-       $_POST = [];
+       $userData = []; 
+    } catch (ValidationException $e) { 
+        $exception = $e;
+        $errors = $e->getErrors(); 
     } catch (Exception $e) {
         $exception = $e;
     }
 }
 
-loadTemplateView('save_user', [
-    "exception" => $exception 
+loadTemplateView('save_user', $userData + [
+    "exception" => $exception,
+    "errors" => $errors
 ]);
--- src/views/save_user.php
+++ src/views/save_user.php
@@ -1,26 +1,29 @@
 <main class="content">
-    <?php 
-        render(
-            'Cadastro de Usuário',
-            'Crie e atualize o usuário',
-            'icofont-user'
-        );
+    <?php
+    render(
+        'Cadastro de Usuário',
+        'Crie e atualize o usuário',
+        'icofont-user'
+    );
 
-        include(TEMPLATES_PATH . '/messages.php');
+    include(TEMPLATES_PATH . '/messages.php');
     ?>
 
     <form action="#" method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="name">Nome</label>
-                <input type="text" name="name" id="name" placeholder="Informe o nome" class="form-control <?= $errors['name'] ? 'is_invalid' : '' ?>" value="<?= $name ?>">
+                <input type="text" name="name" id="name" placeholder="Informe o nome"
+                    class="form-control <?= $errors['name'] ?? '' ? 'is-invalid' : '' ?>"
+                    value="<?= $name ?? '' ?>">
+
                 <div class="invalid-feedback">
-                    <?= $errors['name'] ?>
+                    <?= $errors['name'] ?? '' ?>
                 </div>
             </div>
             <div class="form-group col-md-6">
                 <label for="email">E-mail</label>
-                <input type="email" name="email" placeholder="Informe o e-mail" class="form-control <?= $errors['email'] ? 'is_invalid' : '' ?>" value="<?= $email ?>">
+                <input type="email" name="email" placeholder="Informe o e-mail" class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>" value="<?= $email ?>">
                 <div class="invalid-feedback">
                     <?= $errors['email'] ?>
                 </div>
@@ -29,14 +32,14 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="password">Senha</label>
-                <input type="password" name="password" id="password" placeholder="Informe a senha" class="form-control <?= $errors['password'] ? 'is_invalid' : '' ?>">
+                <input type="password" name="password" id="password" placeholder="Informe a senha" class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>">
                 <div class="invalid-feedback">
                     <?= $errors['password'] ?>
                 </div>
             </div>
             <div class="form-group col-md-6">
                 <label for="confirm_password">Confirme a senha</label>
-                <input type="password" name="confirm_password" placeholder="Confirmação de senha" class="form-control <?= $errors['confirm_password'] ? 'is_invalid' : '' ?>">
+                <input type="password" name="confirm_password" placeholder="Confirmação de senha" class="form-control <?= $errors['confirm_password'] ? 'is-invalid' : '' ?>">
                 <div class="invalid-feedback">
                     <?= $errors['confirm_password'] ?>
                 </div>
@@ -45,14 +48,14 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="start_date">Data de Admissão</label>
-                <input type="date" name="start_date" id="start_date" class="form-control <?= $errors['start_date'] ? 'is_invalid' : '' ?>" value="<?= $start_date ?>">
+                <input type="date" name="start_date" id="start_date" class="form-control <?= $errors['start_date'] ? 'is-invalid' : '' ?>" value="<?= $start_date ?>">
                 <div class="invalid-feedback">
                     <?= $errors['start_date'] ?>
                 </div>
             </div>
             <div class="form-group col-md-6">
                 <label for="end_date">Data de Desligamento</label>
-                <input type="date" name="end_date" id="end_date" class="form-control <?= $errors['end_date'] ? 'is_invalid' : '' ?>" value="<?= $end_date ?>">
+                <input type="date" name="end_date" id="end_date" class="form-control <?= $errors['end_date'] ? 'is-invalid' : '' ?>" value="<?= $end_date ?>">
                 <div class="invalid-feedback">
                     <?= $errors['end_date'] ?>
                 </div>
@@ -61,7 +64,7 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="is_admin">Administrador?</label>
-                <input type="checkbox" id="is_admin" name="is_admin" class="form-control <?= $errors['is_admin'] ? 'is-invalid' : '' ?>" <?= $is_admin ? 'checked' : '' ?>>
+                <input type="checkbox" id="is_admin" name="is_admin" class="<?= $errors['is_admin'] ? 'is-invalid' : '' ?>" <?= $is_admin ? 'checked' : '' ?>>
                 <div class="invalid-feedback">
                     <?= $errors['is_admin'] ?>
                 </div>
